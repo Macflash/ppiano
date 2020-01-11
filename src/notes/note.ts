7b7d75beaaa1fa35bb256f264e2e52ff65402eeb
Alex Breyfogle <alexbr@microsoft.com>
2020-01-10 21:42:13 -0800

    add lodash

--- src/notes/note.ts
+++ src/notes/note.ts
@@ -1,3 +1,5 @@
+import * as _ from "lodash";
+
 // Notes are just a number, like midi but can be float
 // Middle C is 60 (C3)
 
@@ -7,3 +9,31 @@ export type PianoChord = { [note: number]: boolean };
 
 // Don't really need this yet:
 export type Chord = Note[];
+
+export type Interval = number;
+const minorThird = 4;
+const majorThird = 5;
+const fifth = 7;
+
+export function upMajorThird(note: Note): Note {
+    return note + majorThird;
+}
+
+export function upFifth(note: Note): Note {
+    return note + fifth;
+}
+
+export function MajorChord(root: Note): Chord {
+    return [root, upMajorThird(root), upFifth(root)];
+}
+
+export function toPianoChord(chord: Chord): PianoChord {
+    const pianoChord: PianoChord = {};
+    chord.forEach(note => pianoChord[note] = true);
+    return pianoChord;
+}
+
+export function chordsAreEqual(pianoChord: PianoChord, chord: Chord): boolean {
+    const otherChord = toPianoChord(chord);
+    return _.isEqual(pianoChord, otherChord);
+}
