2f3e52dfde74be89b8b597fb1db605087e7138e8
Alex Breyfogle <alexbr@microsoft.com>
2020-01-11 06:52:04 -0800

    Add note dragging

--- src/piano/piano.tsx
+++ src/piano/piano.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { PianoChord, Note } from '../notes/note';
 
-type Key = "W" | "B";
-
-export const Piano: React.FC<{ chord?: PianoChord, onNoteClicked?: (note: Note) => void, flex?: "none" | "auto" }> = props => {
+/** Piano Bed Component */
+export const Piano: React.FC<PianoProps> = props => {
     const { onNoteClicked, flex, chord } = props;
     const baseNote = 60; // Middle C. This is hack and should be adjustable
     const octaves = 2;
@@ -37,10 +36,27 @@ export const Piano: React.FC<{ chord?: PianoChord, onNoteClicked?: (note: Note) 
     </div>
 }
 
-export const PianoKey: React.FC<{ note: Note, keyType: Key, selected?: boolean, onNoteClicked?: (note: Note) => void, width: number }> = props => {
+/** Piano Key Component */
+export const PianoKey: React.FC<PianoKeyProps> = props => {
     const { note, keyType, selected, onNoteClicked, width } = props;
 
-    const onClick = React.useCallback(() => { onNoteClicked?.(note) }, [note, onNoteClicked]);
+    const onClick = React.useCallback(() => { onNoteClicked?.([note]) }, [note, onNoteClicked]);
+
+    const onDragStart = React.useCallback<GameDragEvent>(ev => {
+        ev.persist();
+        ev.dataTransfer.setData("text/plain", note.toString());
+        ev.dataTransfer.dropEffect = "move";
+    }, [note]);
+
+    const onDrop = React.useCallback((ev: React.DragEvent<HTMLDivElement>) => {
+        ev.persist();
+        ev.preventDefault();
+        ev.stopPropagation();
+        const dropppedNote = parseInt(ev.dataTransfer.getData("text/plain"));
+        if (dropppedNote !== note) {
+            onNoteClicked?.([note, dropppedNote]);
+        }
+    }, [note, onNoteClicked]);
 
     const style = React.useMemo(() => ({
         width: keyType === "W" ? `calc(${100 / width}%)` : `calc(${50 / width}%)`,
@@ -52,5 +68,39 @@ export const PianoKey: React.FC<{ note: Note, keyType: Key, selected?: boolean, 
         height: keyType === "W" ? "100%" : "65%",
     }), [width, keyType, selected]);
 
-    return <div key={note} className="pianoKey" style={style} onClick={onClick}></div>
+    return <div
+        key={note}
+        className="pianoKey"
+        style={style}
+        onClick={onClick}
+        draggable={!!onNoteClicked}
+        onDragStart={onDragStart}
+        onDragOver={onDragOverMovableArea}
+        onDrop={onDrop}
+    ></div>
+}
+
+export type GameDragEvent = (ev: React.DragEvent<HTMLDivElement>) => void;
+
+export const onDragOverMovableArea = (ev: React.DragEvent<HTMLDivElement>) => {
+    ev.preventDefault();
+    ev.dataTransfer.dropEffect = "move";
+}
+
+export type onNoteClicked = (notes: Note[]) => void;
+
+export type Key = "W" | "B";
+
+export interface PianoKeyProps {
+    note: Note;
+    keyType: Key;
+    selected?: boolean;
+    onNoteClicked?: onNoteClicked;
+    width: number;
+}
+
+export interface PianoProps {
+    chord?: PianoChord;
+    onNoteClicked?: onNoteClicked;
+    flex?: "none" | "auto";
 }
--- src/piano/simpleClickerPiano.tsx
+++ src/piano/simpleClickerPiano.tsx
@@ -1,37 +1,39 @@
 import React from 'react';
-import { PianoChord, CommonChords, chordsAreEqual, lookupChord, standardize, getNoteName } from '../notes/note';
-import { Piano } from './piano';
+import { PianoChord, CommonChords, chordsAreEqual, lookupChord, standardize, getNoteName, Chord } from '../notes/note';
+import { Piano, onNoteClicked } from './piano';
 import { playPiano } from '../sounds/playSound';
 
-export const SimpleClickerPiano: React.FC = () => {
+function usePianoChord(): [PianoChord, onNoteClicked, React.Dispatch<React.SetStateAction<PianoChord>>] {
     const [chord, setChord] = React.useState<PianoChord>({});
+    const noteClicked = React.useCallback<onNoteClicked>(notes => {
+        let newChord = { ...chord };
+        notes.forEach(note => {
+            newChord[note] = !chord[note];
+            if (!chord[note]) playPiano(note);
+            else delete newChord[note];
+        });
+
+        setChord(newChord);
+    }, [setChord, chord]);
+
+    return [chord, noteClicked, setChord];
+}
+
+export const SimpleClickerPiano: React.FC = () => {
+    const [chord, noteClicked] = usePianoChord();
+
     return (
-        <Piano chord={chord} onNoteClicked={note => {
-            !chord[note] && playPiano(note);
-            let newChord = { ...chord, [note]: !chord[note] };
-            if (chord[note]) {
-                delete newChord[note];
-            }
-            setChord(newChord);
-        }} />
+        <Piano chord={chord} onNoteClicked={noteClicked} />
     );
 }
 
 
 export const SimpleClickerPianoIdentifier: React.FC = () => {
-    const [chord, setChord] = React.useState<PianoChord>({});
-    // if it matches any common chords
+    const [chord, noteClicked, setChord] = usePianoChord();
     return (
         <div style={{ display: "flex", flexDirection: "row" }}>
-            <Piano flex="auto" chord={chord} onNoteClicked={note => {
-                !chord[note] && playPiano(note);
-                let newChord = { ...chord, [note]: !chord[note] };
-                if (chord[note]) {
-                    delete newChord[note];
-                }
-                setChord(newChord);
-            }} />
-            <div style={{ minWidth: 80, display: "flex", flexDirection: "column", justifyContent: "center", flex: "none", paddingRight: 10 }}>
+            <Piano flex="auto" chord={chord} onNoteClicked={noteClicked} />
+            <div style={{ width: 80, display: "flex", flexDirection: "column", justifyContent: "center", flex: "none", paddingRight: 10 }}>
                 <button onClick={() => setChord({})}>Reset</button>
                 {standardize(chord).map(getNoteName).join(", ")}<br />
                 {lookupChord(chord).join(", ")}
